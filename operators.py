cad4d60bdfdc016177577972c724a5ba8cef9f72
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-07-22 13:09:34 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,31 @@
+# arithmetic operators
+x = 9
+y = 7
+print(x+y)
+print(x-y)
+print(x/y)
+print(x*y)
+print(x+81)
+
+# assignment operator
+# a = x+=3
+# print(increament is "a")
+
+b,c = 3,2
+print(c)
+
+# relational operator
+print(x==y)
+print(x<y)
+print(x>y)
+print(x<=y)
+print(x>=y)
+
+
+# logical operator
+print(x==9 and y>2)
+print(x==9 or y>2)
+
+# unary operator
+x = -x
+print(x)
