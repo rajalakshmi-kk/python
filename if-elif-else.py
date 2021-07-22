8c6aa342312377084583d3397d002b7d15da9ef2
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-07-22 15:51:44 +0530

    Add files via upload

---
+++ if-elif-else.py
@@ -0,0 +1,13 @@
+x=16
+r=x%2
+
+if r==0:
+    print('even')
+    if x > 10:
+        print('Greater')
+    elif x < 10:
+        print('lesser')
+# elif r==1:
+#     print('odd')
+else:
+    print('end')
---
+++ while_loop.py
@@ -0,0 +1,11 @@
+# increament
+i=1
+while i<=6:
+    print('while loop')
+    i=i+1
+
+# decreament
+i=6
+while i>=1:
+    print('while loop')
+    i=i-1
