6394d9f4931b4fa49eab8a9ad1a586f32f6977b2
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-07-23 20:01:28 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,8 @@
+D={1:'dairy milk',2:'snicker',3:'milky bar',4:'gone mad',5:'munch'}
+print(D)
+print(D[1])
+print(D.get('gone mad'))
+
+num=['d','s','m','g','mh']
+name=['dairy milk','snicker','milky bar','gone mad','munch']
+print(dict(zip(num,name)))
---
+++ List.py
@@ -0,0 +1,25 @@
+List = [1,2,3,4,5,6,7,8,9,]
+print(List)
+print(List[1])
+print(List[-1])
+print(List[3:8])
+print(List[ : ])
+
+Lang=['Python','R','html & css']
+print(Lang)
+
+details=['python', 3.9, 2021]
+print(details)
+
+lang_details=[Lang,details]
+print(lang_details)
+
+List.insert(9,99)
+print(List)
+print(min(List))
+print(max(List))
+print(sum(List))
+
+num=[3,8,1,9,0,4]
+num.sort()
+print(num)
