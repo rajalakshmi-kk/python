795373b1c91fa0a96f30757e32ec2d5eb99e167f
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-07-23 12:37:26 +0530

    Add files via upload

---
+++ string-operations.py
@@ -0,0 +1,18 @@
+a='self'
+b='love'
+# str concatenation
+print(a+ ' ' +b)
+
+# str multiplication
+print(b * 3)
+
+# string interpolation
+print("always be happy with your %s %s itself !"%(a,b))
+
+# another method for str interpolation
+print(f'always be happy with your {a} {b} itself !')
+
+# Str formatting
+name='Raji'
+print('Hello, {}'.format(name))
+
