d93ae5a27aa7e3a0c8106ac9b2e64b046a0e3292
RAJALAKSHMI K K <78135987+rajalakshmi10042001@users.noreply.github.com>
2021-07-23 10:49:12 +0530

    Add files via upload

---
+++ string-methods.py
@@ -0,0 +1,13 @@
+a="pycharm"
+b="to do 2night"
+print(a.capitalize())
+print(a.count("a"))
+print(a.find("p"))
+print(a.index("m"))
+# isalpha is to check all the char in a string is a char or not
+print(b.isalpha())
+print(a.isalnum())
+print(a.replace('y','i'))
+print(a.replace('i','y'))
+print(a.strip())
+print(b.split('do'))
